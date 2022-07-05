ec204befb8b77fd718c17748f708bb87c9e9266c
Henry Choi <hgstyle@gmail.com>
2022-07-05 17:32:02 +0900

    UTF-8 encoded

--- webapp/SQLtest.py
+++ webapp/SQLtest.py
@@ -1,41 +1,41 @@
-# pip install psycopg2
+Ôªø# pip install psycopg2
 import psycopg2
 
 # connect db
-# conn = psycopg2.connect("host=localhost dbname=test user=postgres password=pwtest port=5432")
-conn = psycopg2.connect(host='localhost', dbname='test1', user='postgres', password='sork1emd', port='5432')
+# conn = psycopg2.connect("host=localhost dbname=test1 user=postgres password=pwtest port=5432")
+conn = psycopg2.connect(dbname='test1', user='postgres', password='sork1emd', host='localhost', port=5432)
 
 # create cursor before a execution
-#cur = conn.cursur()
+cur = conn.cursor()
 
 # execution: create a table
-#cur.execute("CREATE TABLE test_table (title varchar, content text);") 
+cur.execute("CREATE TABLE test_table (title varchar, content text);") 
 
 # execution: input data
-#cur.execute("INSERT INTO test_table (title, content) VALUES (%s, %s)", ('hello' ,'qwerttyfdas'))
+cur.execute("INSERT INTO test_table (title, content) VALUES (%s, %s)", ('hello' ,'qwerttyfdas'))
 
 # example 1
-#cur.execute("INSERT INTO numbers VALUES (%d)", (42,)) # WRONG
-#cur.execute("INSERT INTO numbers VALUES (%s)", (42,)) # correct
+# cur.execute("INSERT INTO numbers VALUES (%d)", (42,)) # WRONG
+cur.execute("INSERT INTO numbers VALUES (%s)", (42,)) # correct
 
 # example 2
-#cur.execute("INSERT INTO foo VALUES (%s)", "bar")    # WRONG
-#cur.execute("INSERT INTO foo VALUES (%s)", ("bar"))  # WRONG
-#cur.execute("INSERT INTO foo VALUES (%s)", ("bar",)) # correct
-#cur.execute("INSERT INTO foo VALUES (%s)", ["bar"])  # correct
+# cur.execute("INSERT INTO foo VALUES (%s)", "bar")    # WRONG
+# cur.execute("INSERT INTO foo VALUES (%s)", ("bar"))  # WRONG
+cur.execute("INSERT INTO foo VALUES (%s)", ("bar",)) # correct
+cur.execute("INSERT INTO foo VALUES (%s)", ["bar"])  # correct
 
 # exampel 3
-#SQL = "INSERT INTO authors (name) VALUES (%s);" # Note: no quotes
-#data = ("O'Reilly", )
-#cur.execute(SQL, data) # Note: no % operator
+SQL = "INSERT INTO authors (name) VALUES (%s);" # Note: no quotes
+data = ("O'Reilly", )
+cur.execute(SQL, data) # Note: no % operator
 
-# test ≈◊¿Ã∫Ì¿« ∏µÁ µ•¿Ã≈Õ∏¶ ∞°¡Æø¿∞Ì √‚∑¬«—¥Ÿ
-#cur.execute("SELECT * FROM test;")
-#print(cur.fetchone())
+# test ÌÖåÏù¥Î∏îÏùò Î™®Îì† Îç∞Ïù¥ÌÑ∞Î•º Í∞ÄÏ†∏Ïò§Í≥† Ï∂úÎ†•ÌïúÎã§
+cur.execute("SELECT * FROM test;")
+print(cur.fetchone())
 
-# µ•¿Ã≈Õ∏¶ ∫Ø∞Ê«ﬂ¥Ÿ∏È π›µÂΩ√ .commit() «ÿ¡÷æÓæﬂ «—¥Ÿ
-#conn.commit()
+# Îç∞Ïù¥ÌÑ∞Î•º Î≥ÄÍ≤ΩÌñàÎã§Î©¥ Î∞òÎìúÏãú .commit() Ìï¥Ï£ºÏñ¥Ïïº ÌïúÎã§
+conn.commit()
 
-# ƒøº≠∏¶ ¥›∞Ì ø¨∞…¿ª ¡æ∑·«—¥Ÿ.
-#cur.close()
-#conn.close()
+# Ïª§ÏÑúÎ•º Îã´Í≥† Ïó∞Í±∏ÏùÑ Ï¢ÖÎ£åÌïúÎã§.
+cur.close()
+conn.close()
